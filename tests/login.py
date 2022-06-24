baa34a8b04f6a0d38b7fdf7ca9012da36193579c
Pedro Samuel Fagundez <samuelfagundez97@gmail.com>
2022-06-24 00:28:19 -0400

    WIP Tests

--- tests/login.py
+++ tests/login.py
@@ -1,16 +1,24 @@
 # ------------------------ IMPORTS ----------------------------- #
 import unittest
+import requests
 
 # ------------------------ SUITE ----------------------------- #
 class TestLogin(unittest.TestCase):
   def test_login_admin(self):
     '''[SUCCESS] You can always log in, as the default admin user.'''
-    self.assertTrue(True)
+    response = requests.post(
+        'http://localhost:5000/auth/login',
+        data={
+          'username': 'admin',
+          'password': 'admin',
+        },
+      )
+    self.assertTrue(response.status_code == 200)
 
-  def test_fail_login_user(self):
-    '''[FAIL] You can't log in with an invalid username.'''
-    self.assertTrue(True)
+  # def test_fail_login_user(self):
+  #   '''[FAIL] You can't log in with an invalid username.'''
+  #   self.assertTrue(True)
 
-  def test_fail_login_password(self):
-    '''[FAIL] You can't log in with an invalid username.'''
-    self.assertTrue(True)
+  # def test_fail_login_password(self):
+  #   '''[FAIL] You can't log in with an invalid username.'''
+  #   self.assertTrue(True)
