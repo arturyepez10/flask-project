bed5d27e1589bde1fc8bfed22439164335f36d41
Pedro Samuel Fagundez <samuelfagundez97@gmail.com>
2022-06-24 15:05:35 -0400

    Including login stronger auth tests

--- tests/admin.py
+++ tests/admin.py
@@ -1,19 +1,54 @@
 # ------------------------ IMPORTS ----------------------------- #
 import unittest
+import requests
+import json
 
 # ------------------------ SUITE (ADMIN USERS) ----------------------------- #
 class TestAdminUsers(unittest.TestCase):
   def test_list_users(self):
     '''[SUCCESS] You can get the list of users in the system.'''
-    self.assertTrue(True)
+    response = requests.get(
+        'http://localhost:5000/admin/users',
+        headers={
+          'x-access-token': 'admin' + ' ' + 'admin'
+        }
+      )
+    self.assertTrue(response.status_code == 200)
 
-  # def test_success_edit_user(self):
-  #   '''[SUCCESS] You can edit a user in the system'''
-  #   self.assertTrue(True)
+  def test_success_edit_user(self):
+    '''[SUCCESS] You can edit a user in the system'''
+    response = requests.put(
+        'http://localhost:5000/admin/users/' + "1",
+        json={
+          'username': 'admin',
+          'name': 'new name',
+          'last_name': '',
+          'role': "admin"
+        },
+        headers={
+          'x-access-token': 'admin' + ' ' + 'admin'
+        }
+    )
+    self.assertTrue(response.status_code == 200)
+    self.assertTrue(response.text == "User updated.")
 
-  # def test_fail_edit_user_id(self):
-  #   '''[FAIL] You can't edit the ID of an user.'''
-  #   self.assertTrue(True)
+  def test_fail_edit_user_id(self):
+    '''[FAIL] You can't edit the ID of an user.'''
+    response = requests.put(
+      'http://localhost:5000/admin/users/' + "1",
+        json={
+          'user-id': 'not-admin',
+          'username': '',
+          'name': '',
+          'last_name': '',
+          'role': ""
+        },
+        headers={
+          'x-access-token': 'admin' + ' ' + 'admin'
+        }
+    )
+    self.assertTrue(response.status_code == 200)
+    self.assertTrue(response.text == "User updated.")
 
   # def test_fail_edit_repeat_username(self):
   #   '''[FAIL] You can't edit the username of a user with another taken user.'''
