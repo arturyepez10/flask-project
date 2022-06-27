ad70ad40debe95657a54db8b5f0915e48e77c3b1
Arturo Yepez <arturyepez10@gmail.com>
2022-06-27 02:44:11 -0400

    analist suite (empty)

---
+++ tests/analist.py
@@ -0,0 +1,8 @@
+# ------------------------ IMPORTS ----------------------------- #
+import unittest
+import requests
+import json
+
+# ------------------------ SUITE (ANALIST USERS) ----------------------------- #
+class TestAnalistUsers(unittest.TestCase):
+  pass
